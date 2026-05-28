b8dbeb827ed1cf7e22bfeda84a4a64d044bb7b11
Urieel <urielsalazar.dev@gmail.com>
2026-05-28 16:25:50 -0600

    Add global throttling

--- skate/settings.py
+++ skate/settings.py
@@ -115,6 +115,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    ,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',  
+        'user': '1000/day',  
+    },
 }
 
 AUTH_USER_MODEL='skate.User'
